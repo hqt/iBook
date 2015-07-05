389c2e942407d0e7abb95f70f4485d1c1f5acc16
Quan Vu <nongdenchet8435848@gmail.com>
2015-07-05 23:57:12 +0700

    Change the label text using code

--- ibook-ios/ibook-ios/ViewController.swift
+++ ibook-ios/ibook-ios/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var iBookLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        iBookLabel.text = "APIDEZ"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
