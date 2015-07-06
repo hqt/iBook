557a7919ee173dbe6dca6523aa137eb3e33f9999
Quan Vu <nongdenchet8435848@gmail.com>
2015-07-06 10:30:19 +0700

    Add pods including Typhoon and AFNetworking, configure with swift

---
+++ ibook-ios/ibook-ios/Bridging.h
@@ -0,0 +1,15 @@
+//
+//  Bridging.h
+//  Table Cells
+//
+//  Created by nongdenchet on 7/5/15.
+//  Copyright (c) 2015 apidez. All rights reserved.
+//
+
+#ifndef Table_Cells_Bridging_h
+#define Table_Cells_Bridging_h
+
+#import <AFNetworking/AFNetworking.h>
+#import <Typhoon/Typhoon.h>
+
+#endif
